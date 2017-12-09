f2b3c6023314daef22c1d5069716547081baa10d
Vivek Singh <viveks@akamai.com>
2017-12-10 02:55:07 +0530

    first commit

---
+++ subscribe.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"github.com/aws/aws-sdk-go/service/sns"
+ 	"github.com/aws/aws-sdk-go/aws"
+ 	"github.com/aws/aws-sdk-go/aws/session"
+	"fmt"
+)
+const endPoint = "https://39cb8fd6.ngrok.io"
+const protocol = "https"
+const topicARN = "arn:aws:sns:ap-south-1:725344396561:open-faas-test"
+
+
+func subscribe(endPoint string, protocol string, topicARN string) {
+	
+	input := &sns.SubscribeInput{
+		Endpoint: &endPoint,
+		Protocol: &protocol,
+		TopicArn: &topicARN,
+	
+	 }
+
+	sess, err := session.NewSession(&aws.Config{Region: aws.String("ap-south-1"),})
+	if(err != nil) {
+		fmt.Printf("Unable to initiate session")
+		return 
+	}
+	svc := sns.New(sess)
+
+	out, err := svc.Subscribe(input)
+	if(err != nil){
+		fmt.Println("Unable to Subscribe")
+		return 
+	}
+	fmt.Printf(*out.SubscriptionArn)
+}
+
+func main() {
+	subscribe(endPoint, protocol, topicARN)
+}
---
+++ subscriber/subscriber.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "io/ioutil"
+    "encoding/json"
+)
+
+const subConfrmType = "SubscriptionConfirmation"
+const notificationType = "Notification"
+
+func confirmSubscription(subcribeURL string) {
+    response, err := http.Get(subcribeURL)
+    if(err != nil) {
+        fmt.Printf("Unbale to confirm subscriptions")
+    } else {
+        fmt.Printf("Subscription Confirmed sucessfully. %d", response.StatusCode)
+    }
+
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    var f interface{}
+    body, err := ioutil.ReadAll(r.Body)
+    if(err != nil){
+        fmt.Printf("Unable to Parse Body")
+    }
+
+    err = json.Unmarshal(body, &f)
+    if(err != nil){
+        fmt.Printf("Unable to Unmarshal request")
+    }
+
+    data := f.(map[string]interface{})
+    fmt.Println(data["Type"].(string))
+    
+    if data["Type"].(string) == subConfrmType {
+        subcribeURL := data["SubscribeURL"].(string)
+        go confirmSubscription(subcribeURL)
+    } else if(data["Type"].(string) == notificationType){
+        fmt.Println("Recieved this message : ", data["Message"].(string))
+    }
+    
+    fmt.Fprintf(w, "Sucess")
+}
+
+func main() {
+    http.HandleFunc("/", handler)
+    http.ListenAndServe(":8081", nil)
+}
+
