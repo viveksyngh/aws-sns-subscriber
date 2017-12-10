ca616b535d844dd23fe1dd93afd54f9290165b70
Vivek Singh <vivekkmr45@yahoo.in>
2017-12-10 16:06:18 +0530

    constants added as environment variable

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
 # aws-sns-subscriber
 
-## Set below os environment variable for AWS credentials
+## Set os environment variable for AWS credentials
 
 ```
 AWS_ACCESS_KEY_ID=<aws-access-key-id>
 AWS_SECRET_ACCESS_KEY=<aws-secret-access-key>
+
+```
+
+## Set os environment varibale for SNS Subscriber or edit constants in ```subscribe.go```
+
+```
+AWS_REGION=<AWS-regio-of-SNS>
+HTTP_ENDPOINT=<HTTP-endpoint-of-subsciber>
+HTTP_PROTOCOL=<HTTP-Protocol-of-subscriber>
+SNS_TOPIC_ARN=<ARN-of-AWS-topic>
 ```
--- subscribe.go
+++ subscribe.go
@@ -5,10 +5,13 @@ import (
  	"github.com/aws/aws-sdk-go/aws"
  	"github.com/aws/aws-sdk-go/aws/session"
 	"fmt"
+	"os"
 )
-const endPoint = "https://39cb8fd6.ngrok.io"
-const protocol = "https"
-const topicARN = "arn:aws:sns:ap-south-1:725344396561:open-faas-test"
+
+const httpEndpoint = "https://39cb8fd6.ngrok.io"
+const httpProtocol = "https"
+const snsTopicARN = "arn:aws:sns:ap-south-1:725344396561:open-faas-test"
+const awsRegion = "ap-south-1"
 
 //subscribe sends a subscribe request to SNS topic and initiates the subscritption process
 func subscribe(endPoint string, protocol string, topicARN string) {
@@ -19,8 +22,8 @@ func subscribe(endPoint string, protocol string, topicARN string) {
 		TopicArn: &topicARN,
 	
 	}
-
-	sess, err := session.NewSession(&aws.Config{Region: aws.String("ap-south-1"),})
+	
+	sess, err := session.NewSession(&aws.Config{Region: aws.String(getenv("AWS_REGION", awsRegion)),})
 	if(err != nil) {
 		fmt.Printf("Unable to initiate session")
 		return 
@@ -35,6 +38,18 @@ func subscribe(endPoint string, protocol string, topicARN string) {
 	fmt.Printf(*out.SubscriptionArn)
 }
 
+//getenv returns env varibale if set otherwise returns default value
+func getenv(key, defaultValue string) string {
+	value := os.Getenv(key)
+	if(len(value) == 0){
+		return defaultValue
+	}
+	return value
+}
+
 func main() {
-	subscribe(endPoint, protocol, topicARN)
+	httpEndpoint := getenv("HTTP_ENDPOINT", httpEndpoint)
+	httpProtocol := getenv("HTTP_PROTOCOL", httpProtocol)
+	snsTopicARN := getenv("SNS_TOPIC_ARN", snsTopicARN)
+	subscribe(httpEndpoint, httpProtocol, snsTopicARN)
 }
